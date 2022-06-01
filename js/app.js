41f5cd43f7f4dee5e97e9fad61ff25c6158b7ec4
eduardolirainfo <eduardolirainfo@gmail.com>
2022-06-01 19:02:13 -0300

    add loadDetails()

--- js/app.js
+++ js/app.js
@@ -15,36 +15,23 @@ function productItem(product) {
     <div class="product-brands"><span class="product-brand background-brand">${product.brand}</span>
 <span class="product-brand background-price">${(parseFloat(product.price) * 5.5).toFixed(2)}</span></div>
   </section>
-  // CARREGAR OS DETALHES
+  <section class="product-details">
+  ${loadDetails(product)}
+  </section>
 </div>`;
 }
 
 //EXEMPLO DO CÓDIGO PARA OS DETALHES DE UM PRODUTO
 function loadDetails(product) {
-  let details = `<section class="product-details"><div class="details-row">
-        <div>Brand</div>
+
+  let details = ["brand","price","product_type","category","rating"]
+  return Object.entries(product).filter(([name, value]) => details.includes(name)).map(([name, value])=>
+  {
+    `<div class="details-row">
+        <div>${name}</div>
         <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">nyx</div>
+          <div class="details-bar-bg" style="width= 250">${(parseFloat(value) * 5.5).toFixed(2)}</div>
         </div>
-      </div><div class="details-row">
-        <div>Price</div>
-        <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">10.49</div>
-        </div>
-      </div><div class="details-row">
-        <div>Rating</div>
-        <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">5</div>
-        </div>
-      </div><div class="details-row">
-        <div>Category</div>
-        <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250"></div>
-        </div>
-      </div><div class="details-row">
-        <div>Product_type</div>
-        <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">bronzer</div>
-        </div>
-      </div></section>`;
+      </div>`
+  }).join("") 
 }
