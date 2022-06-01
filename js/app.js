ea9e1ebd8ea4779fda9dcb83acd6238b829d1ed7
eduardolirainfo <eduardolirainfo@gmail.com>
2022-06-01 18:51:42 -0300

    add productItem()

--- js/app.js
+++ js/app.js
@@ -5,16 +5,15 @@
 })();
 
 
-//EXEMPLO DO CÓDIGO PARA UM PRODUTO
 function productItem(product) {
-  const item = `<div class="product" data-name="NYX Mosaic Powder Blush Paradise" data-brand="nyx" data-type="bronzer" tabindex="508">
+  const item = `<div class="product" data-name="${product.name}" data-brand="${product.brand}" data-type="${product.product_type}" tabindex="${product.id}">
   <figure class="product-figure">
-    <img src="https://d3t32hsnjxo7q6.cloudfront.net/i/deedb7bd74bda43f062a09aab2ee1ec8_ra,w158,h184_pa,w158,h184.png" width="215" height="215" alt="NYX Mosaic Powder Blush Paradise" onerror="javascript:this.src='img/unavailable.png'">
+    <img src="${product.image_link}" width="215" height="215" alt="${product.name}" onerror="javascript:this.src='img/unavailable.png'">
   </figure>
   <section class="product-description">
-    <h1 class="product-name">NYX Mosaic Powder Blush Paradise</h1>
-    <div class="product-brands"><span class="product-brand background-brand">Nyx</span>
-<span class="product-brand background-price">R$ 57.70</span></div>
+    <h1 class="product-name">${product.name}</h1>
+    <div class="product-brands"><span class="product-brand background-brand">${product.brand}</span>
+<span class="product-brand background-price">${(parseFloat(product.price) * 5.5).toFixed(2)}</span></div>
   </section>
   // CARREGAR OS DETALHES
 </div>`;
